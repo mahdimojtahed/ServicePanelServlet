765a46162a2868cb96b278d96049ed165f6f0bfc
Mahdyar <mahdyarmojtahed@gmail.com>
2022-12-29 11:49:54 +0330

    Initial Commit

--- src/main/java/com/dotin/App.java
+++ src/main/java/com/dotin/App.java
@@ -1,10 +1,14 @@
+package com.dotin;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/App")
 public class App extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
