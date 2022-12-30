99976162dc710e82e7bfeec8f81f4826e071d934
Mahdyar <mahdyarmojtahed@gmail.com>
2022-12-30 15:14:48 +0330

    Initial Commit

--- src/main/java/com/dotin/App.java
+++ src/main/java/com/dotin/App.java
@@ -8,12 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/App")
+@WebServlet(name = "/App", value = "/App")
 public class App extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter pw = resp.getWriter();
-        pw.println("Hello Dotin User !");
+
     }
 }
